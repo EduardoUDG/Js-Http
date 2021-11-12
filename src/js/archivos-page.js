528fb0662c94024b7da3bcade7820e1d54410da7
EduardoUbuntu <eduardo.cdorado@alumnos.udg.mx>
2021-11-12 16:04:03 -0600

    Subir imagen a cloudinary

--- src/js/archivos-page.js
+++ src/js/archivos-page.js
@@ -1,3 +1,4 @@
+import { subirImagen } from "./http-provider";
 
 const body = document.body;
 let inputFile, imgFoto;
@@ -26,7 +27,11 @@ const eventos = () => {
 
   inputFile.addEventListener('change', (event) => {
     const file = event.target.files[0];
-    console.log( file );
+    // console.log( file );
+    subirImagen( file )
+      .then( url => {
+        imgFoto.src= url;
+      });
   });
 
 }
--- src/js/http-provider.js
+++ src/js/http-provider.js
@@ -3,6 +3,8 @@
 const jokeUrl = 'https://api.chucknorris.io/jokes/random';
 const urlUsuarios = 'https://reqres.in/api/users?page=2';
 
+const cloudPreset = 'n3j7wzto';
+const cloudUrl    = 'https://api.cloudinary.com/v1_1/eduardoudg/upload';
 
 const obtenerChiste = async() => {
   try {
@@ -25,9 +27,35 @@ const obtenerUsuarios = async() => {
   return usuarios;
 }
 
+// ArchivoSubir :: File
+const subirImagen = async( ArchivoSubir ) => {
+
+  const formData = new FormData();
+  formData.append('upload_preset', cloudPreset);
+  formData.append('file', ArchivoSubir);
+
+  try {
+    const resp = await fetch( cloudUrl, {
+      method: 'POST',
+      body: formData
+
+    })
+    if( resp.ok ) {
+      const cloudRes = await resp.json();
+      // console.log( cloudRes );
+      return cloudRes.secure_url;
+    } else {
+      throw await resp.json();
+    }
+
+  } catch (error) {
+    throw error;    
+  }
+}
 
 export {
   obtenerChiste,
-  obtenerUsuarios
+  obtenerUsuarios,
+  subirImagen
 }
 
